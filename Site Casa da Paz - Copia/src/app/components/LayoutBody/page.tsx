b46befc5350f0e73ac0991ef363f3df366bd93a2
Wellington <wellingtonjoaquim736@gmail.com>
2024-10-31 21:48:01 -0300

    Site

---
+++ Site Casa da Paz - Copia/src/app/components/LayoutBody/page.tsx
@@ -0,0 +1,13 @@
+export const LayoutBody = () => {
+
+    return (
+
+        <>
+        
+        <h1>bodyyyyyyy</h1>
+        <img src="../src/imagens/" alt="" />
+        </>
+
+    )
+
+}
---
+++ Site Casa da Paz - Copia/src/app/page.tsx
@@ -0,0 +1,21 @@
+import { LayoutFooter } from "./components/LayoutFooter/page";
+import { LayoutHeader } from "./components/LayoutHeader/page";
+import { LayoutBody } from "./components/LayoutBody/page";
+
+export default function Home() {
+  return (
+    <>
+    <LayoutHeader>
+
+    </LayoutHeader>
+    
+    <LayoutBody>
+      
+    </LayoutBody>
+
+    <LayoutFooter>
+
+    </LayoutFooter>
+    </>
+  );
+}
